d98e605d0087979530690a6e1c74daac354f8266
lishengze <lishengz.cool@hotmail.com>
2016-12-21 09:47:18 +0800

    增加了mysite

---
+++ django_web/mysite/config_web.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+#CONSOLE_PATH = "/home/test/console/"
+#CONSOLE_WORK_PATH = CONSOLE_PATH + "scripts/"
+
+PROJECT_NAME = "mysite"
+APP_NAME = "blog"
+
+WORK_BASE = "C:/Users/chen.xiaohong/Desktop/django_web/"
+TEMPLATES_PATH = WORK_BASE + PROJECT_NAME + "/templates"
+IP_ADDR = '127.0.0.1'
+
+
---
+++ django_web/mysite/mysite/urls.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+"""mysite URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.static import serve
+from feed import LatestEntriesFeed
+from blog import views as blog_views
+
+import config_web as cfg_w
+
+admin.autodiscover() 
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+   url(r'root/(?P<path>.*)$', serve,
+   {'document_root': cfg_w.TEMPLATES_PATH}),
+    url(r'^account/login/', blog_views.alogin),
+   	url(r'^account/register/', blog_views.register),
+    url(r'^account/logout/', blog_views.alogout), 
+    url(r'^essay/(?P<eid>\d+)/$',blog_views.essay_details),
+    url(r'^search/$',blog_views.task),
+    url(r'^leavemsg/(?P<eid>\d+)/$',blog_views.leave_comment1),
+    url(r'^(?P<pageNo>\d+)/(?P<etype>\d+)/$',blog_views.index),
+    url(r'^latest/feed/$', LatestEntriesFeed()),
+    url(r'^',blog_views.index),
+]
