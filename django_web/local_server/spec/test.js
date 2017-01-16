445a47a12d52eda614d52e23a912724687c85fa2
lishengze <lishengz.cool@hotmail.com>
2017-01-16 14:52:23 +0800

    解决checkbox 问题

--- django_web/local_server/spec/test.js
+++ django_web/local_server/spec/test.js
@@ -1,38 +1,55 @@
 $(function () {
-    console.log ('This is test!')
-
-    $(':checkbox').each(function() {
-        console.log ($(this).attr('id'))
-    });
+    console.log ('This is class Test!')
 
     var selectedNumb = 0;
-    var sum_checkbox_id = 'checkbox1'
+    var sum_checkbox_id = 'action-toggle'
+	var setNumbSelector = $('.paginator')
+	var testCheckbox = 0;
     $(':checkbox').change(function(){            
+		console.log(++testCheckbox);
         if ($(this).attr('id') === sum_checkbox_id) {
-            if ($(this).is(':checked')) {
-                $(':checkbox').each(function() {
-                    if ($(this).attr('id') !== sum_checkbox_id && !$(this).is(':checked')) {
+            if ($(this).is(':checked')) {				
+                $('.action-select').each(function() {	
+                    if (!$(this).is(':checked')) {
+                    // if ($(this).attr('checked') !== 'checked') {
+						console.log('Is action-toggle checked!')
+						console.log ('')
                         $(this).attr("checked", true);
                         ++selectedNumb;
-                        $('#selectNumb').text(selectedNumb + ' 个被选中');
+                        setNumbSelector.text(selectedNumb + ' 个被选中');
                     }
                 });
-            } else {
-                $(':checkbox').each(function() {
-                    if ($(this).attr('id') !== sum_checkbox_id && $(this).is(':checked')) {
+            } else {				
+                $('.action-select').each(function() {
+                    if ($(this).is(':checked')) {
+                    // if ($(this).attr('checked') === 'checked') {
+						console.log('Is action-toggle unchecked!')
+						console.log ('')
                         $(this).attr("checked", false);
                         --selectedNumb;
-                        $('#selectNumb').text(selectedNumb + ' 个被选中');
+                        setNumbSelector.text(selectedNumb + ' 个被选中');
                     }
                 });                    
             } 
+
+            $('.action-select').each(function() {
+                console.log ($(this).attr('checked'));
+                console.log ($(this).is(':checked'));
+            })
         } else {
             if ($(this).is(':checked')) {
                 ++selectedNumb;
+                $(this).attr("checked", true);
             } else {
                 --selectedNumb;
+                $(this).attr("checked", false);
             }
-            $('#selectNumb').text(selectedNumb + ' 个被选中');
+            setNumbSelector.text(selectedNumb + ' 个被选中');
+
+            $('.action-select').each(function() {
+                console.log ($(this).attr('checked'));
+                console.log ($(this).is(':checked'));
+            })
         } 
     })
 });
