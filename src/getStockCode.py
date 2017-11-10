353e7363d24fef28c417c307f29a6ad62b834740
lishengze <lishengz.cool@hotmail.com>
2017-11-10 17:32:12 +0800

    source

--- src/getStockCode.py
+++ src/getStockCode.py
@@ -6,25 +6,29 @@ from WindPy import *
 import json
 w.start();
 
-#获取上交所A股代码#
-AllShAStock = w.wset("SectorConstituent","date=20151122;sectorId=a001010200000000;field=wind_code");
-if AllShAStock.ErrorCode != 0:
-        print("Get Data failed! exit!")
-        exit()
-#获取深圳交所A股代码#
-AllSzAStock = w.wset("SectorConstituent","date=20151122;sectorId=a001010300000000;field=wind_code");
-if AllSzAStock.ErrorCode != 0:
-        print("Get Data failed! exit!")
-        exit()
+# #获取上交所A股代码#
+# AllShAStock = w.wset("SectorConstituent","date=20151122;sectorId=a001010200000000;field=wind_code");
+# if AllShAStock.ErrorCode != 0:
+#         print("Get Data failed! exit!")
+#         exit()
+# #获取深圳交所A股代码#
+# AllSzAStock = w.wset("SectorConstituent","date=20151122;sectorId=a001010300000000;field=wind_code");
+# if AllSzAStock.ErrorCode != 0:
+#         print("Get Data failed! exit!")
+#         exit()
+
 #获取所有A股代码#
-AllAStock =w.wset("SectorConstituent","date=20151122;sectorId=a001010100000000;field=wind_code");
+# AllAStock =w.wset("SectorConstituent","date=20171105;sectorId=a001010100000000;field=wind_code");
+AllAStock =w.wset("SectorConstituent","sectorId=a001010100000000;field=wind_code");
 if AllAStock.ErrorCode != 0:
     print("Get Data failed! exit!")
     exit()
+print AllAStock
+print len(AllAStock.Data[0])
 
 #保存股票代码到文件中#
-with open('AllSzAStock.json',mode='w') as f:json.dump(AllSzAStock.Data[0],f);
-with open('AllShAStock.json',mode='w') as f1:json.dump(AllShAStock.Data[0],f1);
-with open('AllAStock.json',mode='w') as f2:json.dump(AllAStock.Data[0],f2);
+# with open('AllSzAStock.json',mode='w') as f:json.dump(AllSzAStock.Data[0],f);
+# with open('AllShAStock.json',mode='w') as f1:json.dump(AllShAStock.Data[0],f1);
+# with open('AllAStock.json',mode='w') as f2:json.dump(AllAStock.Data[0],f2);
 
 #使用时使用with open('entry.json', 'r') as f:Data = json.load(f)#
