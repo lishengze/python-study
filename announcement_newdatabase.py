9838b8943dfafe76af158e7da2154806ec9e6bb3
lishengze <lishengz.cool@hotmail.com>
2018-02-05 17:37:16 +0800

    change ann

---
+++ announcement_newdatabase.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+from CONFIG import *
+from toolFunc import *
+from database import Database
+
+class AnnouncementNewDatabase(Database):
+    def __init__(self, id=0, host=DATABASE_HOST, user=DATABASE_USER, pwd=DATABASE_PWD, db=DATABASE_NAME):
+        Database.__init__(self, id, host, user, pwd, db)
+        self.id = id
+
+    def __del__(self):
+        Database.__del__(self)
+
+    def get_create_str(self, table_name):
+        value_str = "(Secode varchar(10) not null, Date int not null, \
+                     Announcement varchar(200) not null Primary Key(Date, Announcement), Href varchar(100))"
+        complete_tablename = u'[' + self.db + '].[dbo].['+ table_name +']'
+        create_str = "create table " + complete_tablename + value_str
+        # print create_str
+        return create_str
+
+    def get_insert_str(self, secode, date, annnouncement):
+        col_str = "(Secode, Date, Announcement, Href)"
+        val_str = "\'" + secode + '\', ' + str(date) + ", \'" \
+                + annnouncement[0] + "\', \'" +  annnouncement[1] + "\'" 
+
+        complete_tablename = u'[' + self.db + '].[dbo].['+ secode +']'
+        insert_str = "insert into "+ complete_tablename + col_str + "values ("+ val_str +")"
+        return insert_str
+
+    def insert_data(self, secode, date, annnouncement):
+        try:
+            insert_str = self.get_insert_str(secode, date, annnouncement)
+            self.changeDatabase(insert_str)
+        except Exception as e:
+            connect_error = "20003"
+            if connect_error in e[1]:
+                connFailedWaitTime = 5
+                print '\n^^^^^20003 connection timed out database insert_data restart! ^^^^^ \n'
+                time.sleep(connFailedWaitTime)
+                self.insert_data(secode, date, annnouncement)       
+            elif "Violation of PRIMARY KEY constraint" not in e[1]:
+                print e
+                raise(e)
--- download_announce.py
+++ download_announce.py
@@ -11,7 +11,7 @@ import sys
 from wind import Wind
 from toolFunc import *
 from announcement_database import AnnouncementDatabase
-
+from announcement_newdatabase import AnnouncementNewDatabase
 
 reload(sys) 
 sys.setdefaultencoding('utf8')
@@ -19,8 +19,8 @@ sys.setdefaultencoding('utf8')
 def print_data(msg, data):
     print "\n", msg, len(data)
     datanumb = 100
-    if len(data) > datanumb:
-        data = data[0:datanumb]
+    # if len(data) > datanumb:
+    #     data = data[0:datanumb]
 
     for item in data:
         print item
@@ -47,62 +47,18 @@ def get_sh_announcement_detail(sh_secode_list):
 
     announcement_info = sel.xpath('//em[@class="pdf-first"]/a/text()').extract()
     href_info = sel.xpath('//em[@class="pdf-first"]/a/@href').extract()  
-    print_data("href_info: ", href_info)
+    # print_data("href_info: ", href_info)
     # print_data("announcement_info: ", announcement_info)
 
-    for announcement in announcement_info:
+    for i in range(len(announcement_info)):
+        announcement = announcement_info[i]
+        href = href_info[i]
         tmp_list = announcement.split('：')
         tmp_secode = tmp_list[0]
         if tmp_secode in sh_secode_list:
-            # if ' ' in tmp_list[1]:
-            #     tmp_list[1].remove(' ')
+            # sh_announcement[tmp_secode].append([tmp_list[1], href])
             sh_announcement[tmp_secode].append(tmp_list[1])
     return sh_announcement
-
-def get_sz_announcement():
-    driver = webdriver.PhantomJS()
-    # driver = webdriver.Chrome()
-    url = "http://disclosure.szse.cn/m/drgg.htm"
-
-    driver.set_page_load_timeout(30)
-    driver.get(url)
-    time.sleep(3)
-
-    driver.find_element_by_id('stockCode').send_keys('002678')
-    driver.find_element_by_name('imageField').click()
-    # time.sleep(2)
-
-    # starttime = driver.find_element_by_id('startTime')
-    # starttime.send_keys('2017-01-01')
-    # endtime = driver.find_element_by_id('endTime')
-    # endtime.send_keys('2018-01-01')
-
-    html = driver.page_source.encode('utf-8')
-    html_response = HtmlResponse(driver.current_url, body=html, encoding='utf-8')
-    sel = scrapy.Selector(html_response)
-
-    announcement_info = sel.xpath('//td[@class="td2"]/a/text()').extract()
-    print_data("announcement_info: ", announcement_info)
-
-    # if sel.xpath('//iframe[@id="i_nr"]'):
-    #     driver.switch_to_frame("i_nr")
-    #     html = driver.page_source.encode('utf-8')
-    #     html_response = HtmlResponse(driver.current_url, body=html, encoding='utf-8')
-
-    #     sel = scrapy.Selector(html_response)
-
-    #     # title = sel.xpath('//title/text()').extract()
-    #     # print_data("title: ", title)
-
-    #     # announcement_info = sel.xpath('//table[@class="ggnr"]//a/text()').extract()
-    #     announcement_info = sel.xpath('//td[@class="td2"]/a/text()').extract()
-    #     # announcement_info = sel.xpath('//td[@class="td2"]')
-    #     # announcement_info = sel.xpath('//div[@id="sortAndPageAfficheDiv"]')
-    #     # announcement_info = sel.xpath('//table[@class="ggnr"]')
-
-    #     print_data("announcement_info: ", announcement_info)
-
-    # return announcement_info
 
 def get_sz_announcement_detail(sz_secode_list):
     driver = webdriver.PhantomJS()
@@ -111,6 +67,7 @@ def get_sz_announcement_detail(sz_secode_list):
     sz_announcement = {}
     driver.set_page_load_timeout(30)
 
+    http_prex = "http://disclosure.szse.cn/m/"
 
     for secode in sz_secode_list:
 
@@ -126,11 +83,15 @@ def get_sz_announcement_detail(sz_secode_list):
         sel = scrapy.Selector(html_response)
         announcement_info = sel.xpath('//td[@class="td2"]/a/text()').extract()    
         href_info = sel.xpath('//td[@class="td2"]/a/@href').extract()  
-        print_data(secode + " announcement_info: ", announcement_info)  
-        print_data(secode + " href_info: ", href_info)  
+        # print_data(secode + " announcement_info: ", announcement_info)  
+        # print_data(secode + " href_info: ", href_info)  
 
+        for i in range(len(href_info)):
+            href_info[i] = http_prex + href_info[i]
+        
+        # sz_announcement[secode] = [announcement_info, href_info]
         sz_announcement[secode] = announcement_info
-        # time.sleep(3)
+        
 
     return sz_announcement
 
@@ -162,6 +123,9 @@ def store_annnouncement(announcement_database_obj):
     announcement_database_obj.completeDatabaseTable(secode_list)
     sz_announcement, sh_announcement = get_announcement(sz_secode_list, sh_secode_list)
 
+    print_dict_data("sh_announcement: ", sh_announcement)
+    print_dict_data("sz_announcement: ", sz_announcement)
+
     count = 0
     date = datetime.datetime.now().strftime("%Y%m%d")
     for secode in secode_list:
@@ -189,7 +153,16 @@ def main():
     announcement_database_obj = AnnouncementDatabase(db=dbname, host=host)    
     store_annnouncement(announcement_database_obj)
 
-                    
+def main_new():
+    global updatetime
+    updatetime = 2 * 60 * 60
+
+    host = "192.168.211.165"
+    dbname = "AnnouncementNew"
+    announcement_database_obj = AnnouncementNewDatabase(db=dbname, host=host)    
+    store_annnouncement(announcement_database_obj)
+
+
 def test_get_sz_announcement_detail():
     sz_secode_list = ["300187", "000564"]
     sz_announcement = get_sz_announcement_detail(sz_secode_list)
@@ -203,3 +176,47 @@ if __name__ == "__main__":
     # get_announcement()
     # test_get_sz_announcement_detail()
 
+# def get_sz_announcement():
+#     driver = webdriver.PhantomJS()
+#     # driver = webdriver.Chrome()
+#     url = "http://disclosure.szse.cn/m/drgg.htm"
+
+#     driver.set_page_load_timeout(30)
+#     driver.get(url)
+#     time.sleep(3)
+
+#     driver.find_element_by_id('stockCode').send_keys('002678')
+#     driver.find_element_by_name('imageField').click()
+#     # time.sleep(2)
+
+#     # starttime = driver.find_element_by_id('startTime')
+#     # starttime.send_keys('2017-01-01')
+#     # endtime = driver.find_element_by_id('endTime')
+#     # endtime.send_keys('2018-01-01')
+
+#     html = driver.page_source.encode('utf-8')
+#     html_response = HtmlResponse(driver.current_url, body=html, encoding='utf-8')
+#     sel = scrapy.Selector(html_response)
+
+#     announcement_info = sel.xpath('//td[@class="td2"]/a/text()').extract()
+#     print_data("announcement_info: ", announcement_info)
+
+#     # if sel.xpath('//iframe[@id="i_nr"]'):
+#     #     driver.switch_to_frame("i_nr")
+#     #     html = driver.page_source.encode('utf-8')
+#     #     html_response = HtmlResponse(driver.current_url, body=html, encoding='utf-8')
+
+#     #     sel = scrapy.Selector(html_response)
+
+#     #     # title = sel.xpath('//title/text()').extract()
+#     #     # print_data("title: ", title)
+
+#     #     # announcement_info = sel.xpath('//table[@class="ggnr"]//a/text()').extract()
+#     #     announcement_info = sel.xpath('//td[@class="td2"]/a/text()').extract()
+#     #     # announcement_info = sel.xpath('//td[@class="td2"]')
+#     #     # announcement_info = sel.xpath('//div[@id="sortAndPageAfficheDiv"]')
+#     #     # announcement_info = sel.xpath('//table[@class="ggnr"]')
+
+#     #     print_data("announcement_info: ", announcement_info)
+
+#     # return announcement_info
