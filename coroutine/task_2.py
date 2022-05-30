b1b477b1f430a5ae34abc47a21ffa0943f0520d1
lsz <lishengz.cool@hotmail.com>
2022-05-30 16:15:33 +0800

    fix basic method

--- coroutine/task_2.py
+++ coroutine/task_2.py
@@ -8,7 +8,13 @@ async def func1():
     await asyncio.sleep(3.6)
     
     print("func1 end!")
-    
+  
+  
+async def func2(a, b):
+    print("func2 start!")
+    await asyncio.sleep(3)
+    print("func2 end!")
+      
 
 async def time_out_main():
     try:
@@ -18,6 +24,28 @@ async def time_out_main():
         
 def test_time_out():
     asyncio.run(time_out_main())
+    
+
+    return a+b
+
+def func2_call_back(future):
+    print("func2_call_back: {0}".format(future.result()))
+
+def test_call_back():
+    loop = asyncio.get_event_loop()
+    
+    task = asyncio.ensure_future(func2(10, 5))
+    task.add_done_callback(func2_call_back)
+    
+    
+    loop.run_until_complete(task)
+    
+    # asyncio.run(task)
+    
+    
+
 
 if __name__ == "__main__":
-    test_time_out()
+    # test_time_out()
+    
+    test_call_back()
